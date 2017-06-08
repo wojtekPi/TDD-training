2e3d41ba8557181d8e766a3c5dd915d294a84398
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2017-06-08 19:47:32 +0200

    Add tests to shop.

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,3 +1,14 @@
+import java.util.List;
+
 public class Shop {
+    private final List<Item> listOfItems;
+
+    public Shop(List<Item> listOfItems) {
+        this.listOfItems = listOfItems;
+    }
+
+    public void addProduct(Item bottleOfWater) {
+        listOfItems.add(bottleOfWater);
+    }
 
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,17 +1,37 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class ShopTest {
+
+    private Shop shop;
+
     @Before
     public void setUp() throws Exception {
     }
 
-    @Test
-    public void shoulBeCreated(){
-        assertThat(new Shop()).isNotNull();
+    @Test //This annotation mark our method as a test.
+    public void shouldCreateNonNullObject(){
+        shop = new Shop(null);
+        assertThat(shop).isNotNull();
     }
+
+    @Test
+    public void shouldBeAbleToAddProductsToShop(){
+        List<Item> listOfItems = new ArrayList<Item>();
+        shop = new Shop(listOfItems);
+        Item bottleOfWater = new Item("Bottle of water", 2);
+
+        shop.addProduct(bottleOfWater);
+        assertThat(listOfItems.get(0)).isNotNull();
+
+    }
+
+
 
 }
